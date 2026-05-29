6d14109f0c1ddb2f1edfed6baa0af4a4e3e036c6
anidorusso <alexis@localeclectic.com>
2026-05-28 21:06:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,35 @@
 const express = require("express");
+const https = require("https");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const STORE = process.env.SHOPIFY_STORE;
+const TOKEN = process.env.SHOPIFY_TOKEN;
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.get("/shopify", (req, res) => {
+  const path = req.query.path;
+  if (!path) return res.status(400).json({ error: "Missing path" });
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  const options = {
+    hostname: STORE,
+    path: `/admin/api/2026-04/${path}`,
+    headers: { "X-Shopify-Access-Token": TOKEN }
+  };
+
+  https.get(options, (shopRes) => {
+    let data = "";
+    shopRes.on("data", chunk => data += chunk);
+    shopRes.on("end", () => {
+      res.header("Content-Type", "application/json");
+      res.send(data);
+    });
+  }).on("error", (e) => res.status(500).json({ error: e.message }));
+});
+
+app.listen(port, () => console.log(`Proxy running on port ${port}`));
